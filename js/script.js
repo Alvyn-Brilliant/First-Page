a0b9dae81febbc3078e490c17983bfebfb9ea8ff
Alvyn-Brilliant <alvyn.brilliant@gmail.com>
2025-08-30 20:35:00 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+let count = 0;
+
+function incrementCounter() {
+    count++;
+    document.getElementById("counter").innerHTML = count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
